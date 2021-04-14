32f6ed42c70788803cecb7e9dc6817addfc5a1fd
Keeferf <61815301+Keeferf@users.noreply.github.com>
2021-04-14 14:35:42 -0400

    added javadoc

--- store/ProductStockContainer.java
+++ store/ProductStockContainer.java
@@ -6,12 +6,29 @@
 package store;
 
 public abstract class ProductStockContainer {
-
+    
+    /**
+     * Abstract method for use in another class
+     * @param p : Product
+     */
     public abstract int getProductQuantity(Product p);
-
+    
+    /**
+     * Abstract method for use in another class
+     * @param p : Product
+     * @param quantity : int
+     */
     public abstract void addProductQuantity(Product p, int quantity);
-
+    
+    /**
+     * Abstract method for use in another class
+     * @param p : Product
+     * @param quantity : int
+     */
     public abstract boolean removeProductQuantity(Product p, int quantity);
-
+    
+    /**
+     * Abstract method for use in another class
+     */
     public abstract int getNumOfProducts();
 }
