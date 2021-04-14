321ff25acec7d7f0c52cf29361dadcdc328d7a12
Keeferf <61815301+Keeferf@users.noreply.github.com>
2021-04-14 14:24:15 -0400

    Update ProductStockPair.java

--- store/ProductStockPair.java
+++ store/ProductStockPair.java
@@ -1,3 +1,8 @@
+//STUDENT NAME: Chia-Yu Liu 
+//STUDENT ID: 100698737     
+//STUDENT NAME: Keefer Belanger
+//STUDENT ID: St# 101152085
+
 package store;
 
 public class ProductStockPair {
@@ -9,5 +14,4 @@ public class ProductStockPair {
         this.product = product;
         this.stock = stock;
     }
-
 }
